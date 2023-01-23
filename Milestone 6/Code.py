1007f36dd8d7b44adab89ca99a3724bbebb00a81
JatNad <91890207+JatNad@users.noreply.github.com>
2023-01-23 16:13:12 +0530

    Add files via upload

---
+++ Milestone 6/Code.py
@@ -0,0 +1,128 @@
+import copy
+
+interest=open('POI.txt','r')
+file1=open('Source.txt','r')
+file2=open('output.txt','w')
+
+class polygon(object):
+    def __init__(self,poi_edges):
+        self.boundary='boundary\n'
+        self.layer=file1.readline()
+        self.datatype=file1.readline()
+        self.coordinates=file1.readline()
+        self.vertices=self.coordinates.split()
+        self.edges=int(self.vertices[1])-1
+        self.edgelength=[]
+        if(self.edges==poi_edges):
+            i=2
+            while(i<len(self.vertices)-2):
+                temp=(int(self.vertices[i])-int(self.vertices[i+2]))**2+(int(self.vertices[i+1])-int(self.vertices[i+3]))**2
+                self.edgelength.append(temp)
+                i+=2
+        self.endel=file1.readline()
+    def paste(self):
+        file2.write(self.boundary)
+        file2.write(self.layer)
+        file2.write(self.datatype)
+        file2.write(self.coordinates)
+        file2.write(self.endel)
+
+class POI(object):
+    def __init__(self):
+        self.boundary='boundary\n'
+        self.layer=interest.readline()
+        self.datatype=interest.readline()
+        self.coordinates=interest.readline()
+        self.vertices=self.coordinates.split()
+        self.edges=int(self.vertices[1])-1
+        self.edgelength=[]
+        self.dotproduct=[]
+        i=2
+        while(i<len(self.vertices)-2):
+            temp=(int(self.vertices[i])-int(self.vertices[i+2]))**2+(int(self.vertices[i+1])-int(self.vertices[i+3]))**2
+            self.edgelength.append(temp)
+            i+=2
+        # if(self.edges%2==0):
+        #     i=4
+        #     while(i<len(self.vertices)-2):
+        #         x1=int(self.vertices[i])-int(self.vertices[i-2])
+        #         x2=int(self.vertices[i])-int(self.vertices[i+2])
+        #         y1=int(self.vertices[i+1])-int(self.vertices[i-1])
+        #         y2=int(self.vertices[i+1])-int(self.vertices[i+3])
+        #         self.dotproduct.append(abs(x1*x2+y1*y2))
+        #         i+=2
+        self.endel=interest.readline()
+
+def first():
+    while(True):
+        x=file1.readline()
+        if(x=='boundary\n'):
+            return            
+        else:
+            file2.write(x)
+
+def getpoi():
+    while(True):
+        x=interest.readline()
+        if(x=='boundary\n'):
+            object=POI()
+            return object
+
+def check(poi,poi2,poi3,object):
+    if(poi.edges==object.edges):
+        temp=object.edgelength.index(max(object.edgelength))
+        object.edgelength=object.edgelength[temp:]+object.edgelength[:temp]
+        if(poi.edgelength==object.edgelength):
+            object.paste()
+        elif(poi2.edgelength==object.edgelength):
+            object.paste()
+        elif(poi3.edgelength==object.edgelength):
+            object.paste()
+            # object.edgelength.reverse()
+            # temp=object.edgelength.index(max(object.edgelength))
+            # object.edgelength=object.edgelength[temp:]+object.edgelength[:temp]
+            # if(poi.edgelength==object.edgelength):
+            #     object.paste()
+            #     print(object.edgelength)
+            # if(poi.edges%2==1):
+            #     object.paste()
+            # else:
+            #     dotproduct=[]
+            #     i=4
+            #     while(i<len(object.vertices)-2):
+            #         x1=int(object.vertices[i])-int(object.vertices[i-2])
+            #         x2=int(object.vertices[i])-int(object.vertices[i+2])
+            #         y1=int(object.vertices[i+1])-int(object.vertices[i-1])
+            #         y2=int(object.vertices[i+1])-int(object.vertices[i+3])
+            #         dotproduct.append(abs(x1*x2+y1*y2))
+            #         i+=2
+            #     if(dotproduct==poi.dotproduct):
+            #         print(dotproduct)
+            #         object.paste()
+
+first()
+poi=getpoi()
+temp=poi.edgelength.index(max(poi.edgelength))
+poi.edgelength=poi.edgelength[temp:]+poi.edgelength[:temp]
+interest.close()
+poi2=copy.copy(poi)
+poi3=copy.copy(poi)
+poi2.edgelength=[0.25*i for i in poi.edgelength]
+poi3.edgelength=[2.25*i for i in poi.edgelength]
+
+print(poi.edgelength,poi2.edgelength,poi3.edgelength)
+
+while(True):
+    object=polygon(poi.edges)
+    check(poi,poi2,poi3,object)
+    x=file1.readline()
+    if(x!='boundary\n'):
+        break
+
+file2.write('endstr\n')
+file2.write('endlib')
+
+file1.close()
+file2.close()
+
+#5010
